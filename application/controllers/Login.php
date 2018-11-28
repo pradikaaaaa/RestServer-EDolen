9e3e07aaa2f925d1bff4a200df1ebac52c5fde2a
diosyahrizal <diosyahrizal@gmail.com>
2018-11-28 21:38:35 +0700

    login

---
+++ application/controllers/Login.php
@@ -0,0 +1,35 @@
+<?php
+
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    require APPPATH . '/libraries/REST_Controller.php';
+    use Restserver\Libraries\REST_Controller;
+
+    class Login extends REST_Controller {
+
+        function __construct($config = 'rest') {
+            parent::__construct($config);
+            $this->load->database();
+        }
+
+        public function index_post()
+        {
+            $this->db->select('*');
+            $this->db->from('tbl_user');
+            $this->db->where('username',$this->input->post('username'));
+            $query = $this->db->get();
+            $row = $query->first_row();
+
+            if ($query->num_rows()==1) {
+                return $this->response(array('status' => 'success','result'=>$row));
+            }else{
+                return $this->response(array('status' => 'failed', 502));
+            }
+        }
+
+
+    }
+
+    /* End of file Login.php */
+
+?>
