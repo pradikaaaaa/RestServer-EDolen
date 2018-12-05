08ef911be1fbdfbb9f45579bc443efa0a1b942c3
diosyahrizal <diosyahrizal@gmail.com>
2018-12-05 14:35:37 +0700

    admin page

---
+++ application/controllers/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Admin extends CI_Controller {
+
+
+	    public function __construct()
+		{
+			parent::__construct();
+			$this->load->helper('url','form');
+			$this->load->library('form_validation');
+		}
+
+        public function index()
+        {
+        	$this->load->view('admin/index');
+        }
+
+    }
+
+    /* End of file Admin.php */
+
+?>
