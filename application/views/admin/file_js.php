3137451f11213ebf0fc11bb5d3c92bc316ce6537
pradikaaaaa <pradikanizer13@gmail.com>
2018-12-09 18:37:33 +0700

    perbaikan

---
+++ application/views/admin/file_js.php
@@ -0,0 +1,24 @@
+<!-- jQuery -->
+<script src="<?=base_url()?>assets/vendor/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="<?=base_url()?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="<?=base_url()?>assets/vendor/metisMenu/metisMenu.min.js"></script>
+
+<!-- <script src="<?=base_url()?>assets/ckeditor/ckeditor.js"></script> -->
+
+<!-- Morris Charts JavaScript -->
+<!--
+<script src="assets/admin/vendor/raphael/raphael.min.js"></script>
+<script src="assets/admin/vendor/morrisjs/morris.min.js"></script>
+<script src="assets/admin/data/morris-data.js"></script>
+-->
+<!-- DataTables JavaScript -->
+<script src="<?=base_url()?>assets/vendor/datatables/js/jquery.dataTables.min.js"></script>
+<script src="<?=base_url()?>assets/vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+<script src="<?=base_url()?>assets/vendor/datatables-responsive/dataTables.responsive.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="<?=base_url()?>assets/dist/js/sb-admin-2.js"></script>
--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,21 +1,4 @@
-
-        <!-- jQuery -->
-    <script src="<?php echo base_url()?>assets/vendor/jquery/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="<?php echo base_url()?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="<?php echo base_url()?>assets/vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="<?php echo base_url()?>assets/vendor/raphael/raphael.min.js"></script>
-    <script src="<?php echo base_url()?>assets/vendor/morrisjs/morris.min.js"></script>
-    <script src="<?php echo base_url()?>assets/data/morris-data.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="<?php echo base_url()?>assets/dist/js/sb-admin-2.js"></script>
-    <script src="<?=base_url()?>assets/dist/DataTables/datatables.min.js"></script>
+    <?php $this->load->view('admin/file_js');?>
     <script type="text/javascript">
         $(document).ready(function(){
             $('#example').DataTable({
