1a2c89b99012adb8007824b19ace9f49d88c0d44
diosyahrizal <diosyahrizal@gmail.com>
2018-12-11 14:59:04 +0700

    ambil

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -29,13 +29,7 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <!-- <tr class="odd gradeX">
-                                        <td>Trident</td>
-                                        <td>Internet Explorer 4.0</td>
-                                        <td>Win 95+</td>
-                                        <td class="center">4</td>
-                                        <td class="center">X</td>
-                                    </tr> -->
+                                  
                                 </tbody>
                             </table>
                         </div>
